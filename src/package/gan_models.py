404fc30539c08860af7efa94a22219590e06e860
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2022-04-07 22:25:46 -0400

    presented

--- src/package/gan_models.py
+++ src/package/gan_models.py
@@ -8,18 +8,28 @@ class Generator(nn.Module):
 
     def __init__(self, input_length: int):
         super(Generator, self).__init__()
-        self.dense_layer = nn.Linear(int(input_length), int(input_length))
-        self.activation = nn.Sigmoid()
+        self.dense_layer = nn.Linear(int(input_length)*4, 256)
+        self.hidden1 = nn.Linear(256, 512)
+        self.hidden2 = nn.Linear(512, 512)
+        self.output = nn.Linear(512, int(input_length))
+        self.activation = nn.LeakyReLU()
 
     def forward(self, x):
-        return self.activation(self.dense_layer(x))
+        x = self.activation(self.dense_layer(x))
+        x = self.activation(self.hidden1(x))
+        x = self.activation(self.hidden2(x))
+        return torch.sigmoid(self.output(x))
 
 
 class Discriminator(nn.Module):
     def __init__(self, input_length: int):
         super(Discriminator, self).__init__()
-        self.dense = nn.Linear(int(input_length), 1)
-        self.activation = nn.Sigmoid()
+        self.dense_layer = nn.Linear(int(input_length)*4, 256)
+        self.hidden1 = nn.Linear(256, 512)
+        self.output = nn.Linear(512, int(input_length))
+        self.activation = nn.LeakyReLU()
 
     def forward(self, x):
-        return self.activation(self.dense(x))
+        x = self.activation(self.dense_layer(x))
+        x = self.activation(self.hidden1(x))
+        return torch.sigmoid(self.output(x))
