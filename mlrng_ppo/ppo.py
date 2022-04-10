aa1bac16e00556a4807b65e546ee2970327a9f59
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2022-04-10 18:33:21 -0400

    troubleshooting but still converges to always select 0.5

--- mlrng_ppo/ppo.py
+++ mlrng_ppo/ppo.py
@@ -4,7 +4,13 @@ from network import FeedForwardNN
 from torch.distributions import Bernoulli
 from torch.optim import Adam
 from torch import log_, nn
-import rng_break_env
+try:
+    from RLGym.rng_break_env import RNGEnv
+except:
+    import sys
+    import os
+    sys.path.append('.')
+    from RLGym.rng_break_env import RNGEnv
 
 
 class PPO:
@@ -13,7 +19,7 @@ class PPO:
 
         self.env = env
         self.obs_dim = env.observation_space.shape[0]
-        self.act_dim = env.action_space.shape[0]
+        self.act_dim = 1  # env.action_space.shape[0]
         # ALG STEP 1
         # Initialize actor and critic networks
         self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
@@ -30,8 +36,8 @@ class PPO:
 
     def _init_hyperparameters(self):
         # Default values for hyperparameters, will need to change later.
-        self.timesteps_per_batch = 4800            # timesteps per batch
-        self.max_timesteps_per_episode = 1600      # timesteps per episode
+        self.timesteps_per_batch = 10_000            # timesteps per batch
+        self.max_timesteps_per_episode = 10_000      # timesteps per episode
         self.gamma = 0.95
         self.n_updates_per_iteration = 5
         self.clip = 0.2  # As recommended by the paper
@@ -56,13 +62,12 @@ class PPO:
                 # Calculate ratios
                 ratios = torch.exp(curr_log_probs - batch_log_probs)
                 # Calculate surrogate losses
-                print(A_k.shape)
                 surr1 = ratios * A_k
                 surr2 = torch.clamp(
                     ratios, 1 - self.clip, 1 + self.clip) * A_k
                 actor_loss = (-torch.min(surr1, surr2)).mean()
                 critic_loss = nn.MSELoss()(V, batch_rtgs)
-                print(actor_loss, critic_loss)
+                print(f"ac batch losses {actor_loss, critic_loss}")
 
                 # Calculate gradients and perform backward propagation for actor
                 # network
@@ -73,6 +78,7 @@ class PPO:
                 self.critic_optim.zero_grad()
                 critic_loss.backward()
                 self.critic_optim.step()
+        self.env.render()
 
     def rollout(self):
         # Batch data
@@ -82,6 +88,7 @@ class PPO:
         batch_rews = []            # batch rewards
         batch_rtgs = []            # batch rewards-to-go
         batch_lens = []            # episodic lengths in batch
+        mean_actions = []
         # Number of timesteps run so far this batch
         t = 0
         while t < self.timesteps_per_batch:  # Rewards this episode
@@ -92,7 +99,7 @@ class PPO:
                 # Increment timesteps ran this batch so far
                 t += 1    # Collect observation
                 batch_obs.append(obs)
-                action, log_prob = self.get_action(obs)
+                action, log_prob, mean_action = self.get_action(obs)
                 obs, rew, done, _ = self.env.step(action)
 
                 # Collect reward, action, and log prob
@@ -104,31 +111,29 @@ class PPO:
             batch_lens.append(ep_t + 1)  # plus 1 because timestep starts at 0
             batch_rews.append(ep_rews)
         # Reshape data as tensors in the shape specified before returning
-        batch_obs = torch.tensor(batch_obs, dtype=torch.float)
-        batch_acts = torch.tensor(batch_acts, dtype=torch.float)
+        batch_obs = torch.tensor(np.array(batch_obs), dtype=torch.float)
+        batch_acts = torch.tensor(np.array(batch_acts), dtype=torch.float)
         batch_log_probs = torch.tensor(
             batch_log_probs, dtype=torch.float)  # ALG STEP #4
-        print(batch_log_probs.shape)
         batch_rtgs = self.compute_rtgs(batch_rews)  # Return the batch data
         return batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens
 
     def get_action(self, obs):  # Query the actor network for a mean action.
         # Same thing as calling self.actor.forward(obs)
         mean = self.actor(obs)
-        # Create our Multivariate Normal Distribution
-        # Sample an action from the distribution and get its log prob
+
         dist = Bernoulli(mean)
         action = dist.sample()
-        print(f"a is {mean}")
+        #action = torch.tensor((0,), dtype=torch.float)
         log_prob = dist.log_prob(action).sum()
-
+        #print(   f"selected {mean.detach().cpu().numpy()} with p {log_prob.detach().cpu().numpy()}")
         # Return the sampled action and the log prob of that action
         # Note that I'm calling detach() since the action and log_prob
         # are tensors with computation graphs, so I want to get rid
         # of the graph and just convert the action to numpy array.
         # log prob as tensor is fine. Our computation graph will
         # start later down the line.
-        return action.detach().numpy(), log_prob.detach()
+        return action.detach().numpy(), log_prob.detach(), mean.detach()
 
     def compute_rtgs(self, batch_rews):
         # The rewards-to-go (rtg) per episode per batch to return.
@@ -156,9 +161,19 @@ class PPO:
         log_probs = dist.log_prob(batch_acts).sum()
         return V, log_probs
 
+    def generate(self, n):
+        outputs = []
+        obs = self.env.reset()
+        for _ in range(n):
+            mean = self.actor(obs)
+            action = torch.round(mean).detach()
+            outputs.append(mean.detach().cpu().numpy()[0])
+            self.env.step(action.detach())
+        print(outputs)
+
 
 if __name__ == "__main__":
-
-    env = rng_break_env.RNGEnv()
+    env = RNGEnv()
     model = PPO(env)
-    model.learn(10_000_000)
+    model.learn(100_000)
+    model.generate(10_000)
