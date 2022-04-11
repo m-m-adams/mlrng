4179f5d96ee6c0bd9c660efbe649043fb4bf01eb
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2022-04-11 19:17:55 -0400

    produce report plots

--- dqn_output.py
+++ dqn_output.py
@@ -1,11 +1,16 @@
 import numpy as np
 import nistrng
 import numpy
+import random
 import matplotlib.pyplot as plt
+from sympy import python
 
 dqn_out = np.loadtxt("dqn_output.txt", dtype=np.uint8, delimiter=",")
 xp = np.packbits(dqn_out)
-
+numpy_out = np.random.randint(0, 1, size=100_000)
+np_bits = nistrng.pack_sequence(numpy_out)
+python_rand = np.array([random.randint(0, 1) for _ in range(100_000)])
+py_bits = nistrng.pack_sequence(python_rand)
 plt.xlabel("Output value")
 plt.ylabel("Frequency")
 plt.title('DQN Output frequencies as 8 bit integers')
@@ -15,22 +20,22 @@ plt.savefig("DQNOutputDistribution.png")
 
 def run_tests(seq: np.ndarray):
     eligible_battery: dict = nistrng.check_eligibility_all_battery(
-        dqn_out, nistrng.SP800_22R1A_BATTERY)
+        seq, nistrng.SP800_22R1A_BATTERY)
 
     for name in eligible_battery.keys():
         print("-" + name)
 
-    results = nistrng.run_all_battery(dqn_out, eligible_battery, False)
+    results = nistrng.run_all_battery(seq, eligible_battery, False)
     # Print results one by one
     print("Test results:")
     for result, elapsed_time in results:
         if result.passed:
-            print("- PASSED - score: " + str(numpy.round(result.score, 3)) +
-                  " - " + result.name + " - elapsed time: " + str(elapsed_time) + " ms")
+            print(f"\item PASSED {result.name}")
         else:
-            print("- FAILED - score: " + str(numpy.round(result.score, 3)) +
-                  " - " + result.name + " - elapsed time: " + str(elapsed_time) + " ms")
+            print(f"\item FAILED {result.name}")
 
+
+run_tests(dqn_out)
 # Test results:
 # - FAILED - score: 0.0 - Monobit - elapsed time: 0 ms
 # - PASSED - score: 0.987 - Frequency Within Block - elapsed time: 1 ms
