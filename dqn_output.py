1392cbc0da9c537ef0f3be8afc58988c237f6fcb
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2022-04-10 20:41:00 -0400

    plots for the GAN and DQN outputs

--- dqn_output.py
+++ dqn_output.py
@@ -6,22 +6,30 @@ import matplotlib.pyplot as plt
 dqn_out = np.loadtxt("dqn_output.txt", dtype=np.uint8, delimiter=",")
 xp = np.packbits(dqn_out)
 
-eligible_battery: dict = nistrng.check_eligibility_all_battery(
-    dqn_out, nistrng.SP800_22R1A_BATTERY)
+plt.xlabel("Output value")
+plt.ylabel("Frequency")
+plt.title('DQN Output frequencies as 8 bit integers')
+plt.hist(xp, bins=64, density=True)
+plt.savefig("DQNOutputDistribution.png")
 
-for name in eligible_battery.keys():
-    print("-" + name)
 
-results = nistrng.run_all_battery(dqn_out, eligible_battery, False)
-# Print results one by one
-print("Test results:")
-for result, elapsed_time in results:
-    if result.passed:
-        print("- PASSED - score: " + str(numpy.round(result.score, 3)) +
-              " - " + result.name + " - elapsed time: " + str(elapsed_time) + " ms")
-    else:
-        print("- FAILED - score: " + str(numpy.round(result.score, 3)) +
-              " - " + result.name + " - elapsed time: " + str(elapsed_time) + " ms")
+def run_tests(seq: np.ndarray):
+    eligible_battery: dict = nistrng.check_eligibility_all_battery(
+        dqn_out, nistrng.SP800_22R1A_BATTERY)
+
+    for name in eligible_battery.keys():
+        print("-" + name)
+
+    results = nistrng.run_all_battery(dqn_out, eligible_battery, False)
+    # Print results one by one
+    print("Test results:")
+    for result, elapsed_time in results:
+        if result.passed:
+            print("- PASSED - score: " + str(numpy.round(result.score, 3)) +
+                  " - " + result.name + " - elapsed time: " + str(elapsed_time) + " ms")
+        else:
+            print("- FAILED - score: " + str(numpy.round(result.score, 3)) +
+                  " - " + result.name + " - elapsed time: " + str(elapsed_time) + " ms")
 
 # Test results:
 # - FAILED - score: 0.0 - Monobit - elapsed time: 0 ms
